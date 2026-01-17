fa2099a51d25a383ad41c1f76e4673a54495bfb3
Adrika Tiwari <adrikatiwari11993@gmail.com>
2026-01-17 13:36:04 +0530

    Fix app.run for Render Deployment

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def predict():
         return render_template('index.html', prediction_text="Error: Invalid Input")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
